ccefc2d553488907e314eab64d35f847bc3fb73b
Brian Hague <alphaseinor@gmail.com>
2019-11-22 20:32:38 -0600

    added initialstate from readme, about to modify what it needs

---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,11 @@
+import {
+  FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_FAIL,
+  POST_DATA_START, POST_DATA_SUCCESS, POST_DATA_FAIL
+} from '../actions/actions.js'
+
+const initialState = {
+  name: "Brainey",
+  age: 200,
+  height: "5cm",
+  id: 0
+}
