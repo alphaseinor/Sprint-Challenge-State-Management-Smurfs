b2ea33e4cd5d3c11a4a04dafed43712e00436019
Brian Hague <alphaseinor@gmail.com>
2019-11-22 20:29:06 -0600

    added gargamel function to dispatch smurfs to the api

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -4,6 +4,10 @@ export const FETCH_DATA_START = 'FETCH_DATA_START';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAIL = 'FETCH_DATA_FAIL';
 
+export const POST_DATA_START = 'POST_DATA_START';
+export const POST_DATA_SUCCESS = 'POST_DATA_SUCCESS';
+export const POST_DATA_FAIL = 'POST_DATA_FAIL';
+
 //la LA la la la la la la la la la! make fetch happen!
 
 export const smurfFetch = () => dispatch => {
@@ -19,5 +23,17 @@ export const smurfFetch = () => dispatch => {
       })
 }
 
-// s*** we have to be Gargamel too?... hmmm I guess I could just make them all males and have a stork deliver them... decisions decisions...
+// s*** we have to be Gargamel too?... hmmm I guess I could just make them all males and have a stork dispatch them... decisions decisions...
 
+export const gargamel = (smurf) => dispatch => {
+  dispatch({ type: POST_DATA_START });
+  axios.post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+          console.log(res.data);
+          dispatch({ type: POST_DATA_SUCCESS, payload: smurf });
+      })
+      .catch(error => {
+          console.log(error);
+          dispatch({ type: POST_DATA_FAIL, payload: error });
+      })
+}
