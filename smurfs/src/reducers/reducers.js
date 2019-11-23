9faf881f63f843da4521448293655583f200deb5
Brian Hague <alphaseinor@gmail.com>
2019-11-22 20:37:29 -0600

    messed with postman and found the data is the same... but I love the error message

--- smurfs/src/reducers/reducers.js
+++ smurfs/src/reducers/reducers.js
@@ -3,9 +3,11 @@ import {
   POST_DATA_START, POST_DATA_SUCCESS, POST_DATA_FAIL
 } from '../actions/actions.js'
 
-const initialState = {
-  name: "Brainey",
-  age: 200,
-  height: "5cm",
-  id: 0
-}
+// {
+//   name: "Brainey",
+//   age: 200,
+//   height: "5cm",
+//   id: 0
+// }
+
+
