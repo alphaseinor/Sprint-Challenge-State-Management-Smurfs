e1cf689d72f528964ed2d652d068666aa15d96ce
Brian Hague <alphaseinor@gmail.com>
2019-11-22 21:22:35 -0600

    first draft at Smurfs component, fails because Smurf doesn't exist yet

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {smurfFetch} from '../actions/actions.js'
+
+
+const Smurfs = (props) => {
+  const {smurfs, isFetching, isGargamel} = props
+
+  useEffect(() => {
+    props.smurfFetch();
+  }, [])
+
+  return (
+    <section>
+      {isFetching && <p>Stork Dropping Smurfs</p>}
+      {isFetching && !isGargamel && 
+        smurfs.map(smurf => (
+          <Smurf key={smurf.id} smurf={smurf} />
+        ))
+      }
+    </section>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    isFetching: state.isFetching,
+    isGargamel: state.isGargamel,
+  }
+}
+
+
+export default connect(mapStateToProps, {smurfFetch})(Smurfs)
